c18585653c6b694535f043c32e90dc1a9a38de3f
egorsv1985 <egorvs2009@gmail.com>
2022-10-06 19:37:30 +0300

    fix

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import {HashRouter as Router, Routes, Route, Navigate} from "react-router-dom";
+import {Router, Routes, Route, Navigate} from "react-router-dom";
 import Home from './pages/Home';
 import Projects from './pages/Projects';
 import Experience from './pages/Experience';
@@ -9,18 +9,15 @@ import ProjectDisplay from "./pages/ProjectDisplay";
 
 function App() {
   return (<div className="App">
-    <Router>
       <NavBar />
+      
       <Routes>
-        <Route path="/" element={<Navigate to={"/react-portfolio-website"}/>}/>
-        <Route path="/*" element={<Navigate to={"/react-portfolio-website"}/>}/>
-        <Route path="/react-portfolio-website" element={<Home />}/>
+        <Route path="/" element={<Home />}/>
         <Route path="/projects" element={<Projects />}/>
         <Route path="/projects/:id" element={<ProjectDisplay />}/>
         <Route path="/experience" element={<Experience />}/>
       </Routes>
       <Footer/>
-    </Router>
   </div>
   );
 }

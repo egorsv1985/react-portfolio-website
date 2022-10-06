734c8821f5f8d0c32598218946846cd01f6c8350
egorsv1985 <egorvs2009@gmail.com>
2022-10-06 19:45:56 +0300

    fix

--- src/App.js
+++ src/App.js
@@ -1,28 +1,26 @@
 import "./App.css";
-import {Router, Routes, Route, Navigate} from "react-router-dom";
-import Home from './pages/Home';
-import Projects from './pages/Projects';
-import Experience from './pages/Experience';
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Projects from "./pages/Projects";
+import Experience from "./pages/Experience";
 import NavBar from "./components/NavBar";
 import Footer from "./components/Footer";
 import ProjectDisplay from "./pages/ProjectDisplay";
 
 function App() {
-  return (<div className="App">
+  return (
+    <div className="App">
       <NavBar />
-      
+
       <Routes>
-        <Route path="/" element={<Navigate to={"/react-portfolio-website/"}/>}/>
-        <Route path="/*" element={<Navigate to={"/react-portfolio-website/"}/>}/>
-        <Route path="/react-portfolio-website/" element={<Home />}/>
-        <Route path="/projects" element={<Projects />}/>
-        <Route path="/projects/:id" element={<ProjectDisplay />}/>
-        <Route path="/experience" element={<Experience />}/>
+        <Route path="/" element={<Home />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/projects/:id" element={<ProjectDisplay />} />
+        <Route path="/experience" element={<Experience />} />
       </Routes>
-      <Footer/>
-  </div>
+      <Footer />
+    </div>
   );
 }
 
 export default App;
-
